c017dc4edb2fda2e0df8f3337a25e5a6f2fda692
DESKTOP-E7S6P6O\u <leesj9627@naver.com>
2025-11-19 23:44:09 +0900

    Controller 추가 및 Service코드 수정

--- src/main/java/com/back/controller/PostController.java
+++ src/main/java/com/back/controller/PostController.java
@@ -1,7 +1,20 @@
 package com.back.controller;
-
+import com.back.dto.PostDTO;
 import com.back.service.PostService;
+import java.util.Scanner;
 
 public class PostController {
     public PostService postService;
+    Scanner scanner = new Scanner(System.in);
+    String usercontent = scanner.nextLine();
+    int usercontentnum = scanner.nextInt();
+
+    public void starterController(){
+        if("등록".equals(usercontent)){
+            postService.setPostDTO();
+        }
+        else if("목록".equals(usercontent)){
+            postService.deletePostDTO(usercontentnum);
+        }
+    }
 }
--- src/main/java/com/back/service/PostService.java
+++ src/main/java/com/back/service/PostService.java
@@ -4,33 +4,70 @@ import com.back.dto.PostDTO;
 import com.back.respository.PostRepository;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class PostService {
     public PostRepository postRepository;
+    public PostDTO postDTO;
+    Scanner scanner = new Scanner(System.in);
 
     //등록
-    public void setPostDTO(PostDTO postDTO) {
-        postDTO.setPostnum(postDTO.getPostnum());
-        postDTO.setContent(postDTO.getContent());
-        postDTO.setAuthor(postDTO.getAuthor());
+    public void setPostDTO() {
+        PostDTO postDTO = new PostDTO();
+        int postnum = 0;
+        System.out.print("내용 : ");
+        String content = scanner.next();
+        System.out.print("저자 : ");
+        String author = scanner.next();
+
+        postDTO.setPostnum(postnum);
+        postDTO.setContent(content);
+        postDTO.setAuthor(author);
+
+        System.out.println(postnum+"번 게시글이 등록되었습니다.");
+        postRepository.postDTOList.add(postDTO);
     }
     
     //삭제
-    public void deletePostDTO(PostDTO postDTO) {
-        System.out.println(postDTO.getPostnum()+"번 게시글이 삭제되었습니다.");
-        postDTO = null;
+    public void deletePostDTO(int postnum) {
+        postRepository.postDTOList.get(postnum);
+        if(postRepository.postDTOList.get(postDTO.getPostnum()-1)!=null) {
+            postRepository.postDTOList.remove(postDTO);
+            System.out.println(postDTO.getPostnum()+"번 게시글이 삭제되었습니다.");
+        }
+        else if(postRepository.postDTOList.get(postDTO.getPostnum()-1)==null) {
+            System.out.println(postDTO.getPostnum()+"번 게시글을 찾을 수 없습니다.");
+        }
     }
 
     //목록 (내림차순)
     public void getPostDTOList() {
         for(int i=List.of().size(); i<=0; i--){
+            if(List.of().size() == 0){
+                System.out.println("반환값이 없습니다.");
+                break;
+            }
             System.out.println(postRepository.postDTOList.get(i).getPostnum()+" / "+postRepository.postDTOList.get(i).getAuthor()+" / "+postRepository.postDTOList.get(i).getContent());
         }
     }
 
     //수정
     public void updatePostDTO(PostDTO postDTO) {
-        postDTO.setContent(postDTO.getContent());
-        postDTO.setAuthor(postDTO.getAuthor());
+        if(postRepository.postDTOList.get(postDTO.getPostnum()-1)!=null) {
+            String content = scanner.next();
+            String author = scanner.next();
+            System.out.println("내용(기존) : "+postDTO.getContent());
+            System.out.print("내용 : ");
+            postDTO.setContent(content);
+            System.out.println("작성자(기존) : "+postDTO.getAuthor());
+            System.out.print("작성자 : ");
+            postDTO.setAuthor(author);
+            postRepository.postDTOList.set(postDTO.getPostnum()-1, postDTO);
+        }
+        else if(postRepository.postDTOList.get(postDTO.getPostnum()-1)==null) {
+            System.out.println(postDTO.getPostnum()+"번 게시글을 찾을 수 없습니다.");
+        }
     }
+
+
 }
